2347ab2d8fa8923b96c1548fa1dbae5ded6ab5f3
qaShruthi <reachqashruthi@gmail.com>
2024-03-18 22:20:12 -0400

    added binary search

--- src/simplejava/BinarySearchInArray.java
+++ src/simplejava/BinarySearchInArray.java
@@ -1,9 +1,20 @@
 package simplejava;
 
+import java.util.Arrays;
+
 public class BinarySearchInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Should be in sorted order in order to do binary search
+		
+	        int arr[] = {10,20,30,40,50};  
+	        int key = 50;  
+	        int result = Arrays.binarySearch(arr,key);  
+	        if (result < 0)  
+	            System.out.println("Element is not found!");  
+	        else  
+	            System.out.println("Element is found at index: "+result);  
+	   
 
 	}
 
